a5c16d4ac98926e8d80b3814106c1e3a271daaf8
roy-agung <pamungkasagung847@gmail.com>
2025-12-12 14:52:28 +0700

    add data dummy

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,7 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
+use App\Models\Siswa;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,11 +15,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        // Panggil ProfileSeeder dengan cara yang benar
+        $this->call([
+            ProfileSeeder::class,
+            UserSeeder::class,
+            SiswaSeeder::class
         ]);
     }
 }
